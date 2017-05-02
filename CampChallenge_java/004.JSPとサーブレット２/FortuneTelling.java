306f391637b6168cf966067e63a416168e4caa21
KYAMAZAKI-NOTE\K.Yamazaki <yamazaki0053@gmail.com>
2017-05-02 16:29:05 +0900

    Java／サーブレットとJSPの課題を提出いたします。

---
+++ CampChallenge_java/004.JSPとサーブレット２/FortuneTelling.java
@@ -0,0 +1,142 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.camp.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Random;
+
+/**
+ * サーブレットからJSPを呼び出す準備
+ */
+//占いの処理用のimport
+import java.util.Date;
+import java.util.Random;
+//JSPを呼び出すためのimport
+import javax.servlet.RequestDispatcher;
+//JavaBeans
+import org.camp.servlet.ResultData;
+
+/**
+ *
+ * @author K.Yamazaki
+ */
+public class FortuneTelling extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        
+        //大吉・中吉・小吉・吉・平吉・末吉・末小吉・凶・小凶・半凶・末凶・大凶
+        String luckList[] = {"大吉", "中吉", "小吉", "吉", "半吉", "末小吉", "凶", "小凶", "半凶", "末凶", "大凶"};
+        //乱数クラス生成
+        Random rand = new Random();
+        //乱数取得
+        Integer index = rand.nextInt(luckList.length);
+        
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet FortuneTelling</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>今日のあなたの運勢は。。。" + luckList[index] + "！</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+        
+        /**
+         * 占い結果をリクエストスコープへ設定
+         */
+        //ResultDataのインスタンスを生成
+        ResultData data = new ResultData();
+        //setterで実施日付と運勢を記録
+        data.setD(new Date());
+        data.setLuck(luckList[index]);
+        //占い結果をリクエストスコープに登録
+        request.setAttribute("DATA", data);
+        
+        /**
+         * ServletクラスからJSPへ処理を以降する
+         */
+        final String result = "/WEB-INF/jsp/FortuneTellingResult.jsp";
+        RequestDispatcher rd = request.getRequestDispatcher(result);
+        rd.forward(request, response);
+        
+    }
+//       /**
+//         * 占い結果をリクエストスコープへ設定
+//         */
+//        //ResultDataのインスタンスを生成
+//        ResultData data = new ResultData();
+//        //setterで実施日付と運勢を記録
+//        data.setD(new Date());
+//        data.setLuck(luckList[index]);
+//        //占い結果をリクエストスコープに登録
+//        request.setAttribute("DATA", data);
+//        
+//        /**
+//         * JSPを呼び出す
+//         */
+//        //リクエストディスパッチャーを取得
+//        RequestDispatcher rd = request.getRequestDispatcher("/FortuneTellingResult");
+//        //JSPを呼び出す
+//        rd.forward(request, response);
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
