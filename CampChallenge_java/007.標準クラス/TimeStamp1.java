c8f5e904a4061c30b427e5481ca513ba1c85eeef
KYAMAZAKI-NOTE\K.Yamazaki <yamazaki0053@gmail.com>
2017-05-15 12:59:56 +0900

    Java／標準クラス／日付編(タイムスタンプの表示1)　の課題を提出いたします。

--- CampChallenge_java/007.標準クラス/TimeStamp1.java
+++ CampChallenge_java/007.標準クラス/TimeStamp1.java
@@ -15,9 +15,9 @@ import java.util.Date;
  */
 public class TimeStamp1 {
     public static void main(String[] args) throws ParseException {              //“throws ParseException”は、parseの行で出現するヒントから自動入力した
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");     //SimpleDateFormatのインスタンスを作成
-        Date d = sdf.parse("2016-11-04 10:0:0");     //引数に指定した文字列を解釈して、Dateオブジェクトを返す
-        long timeStamp = d.getTime();       //取得したDate型オブジェクトを整数へ代入
-        System.out.println(timeStamp);
+        Date d = new Date();                    //Dateをインスタンス化
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");     //SimpleDateFormatをインスタンス化
+        String date = sdf.format(d);            //Date型を文字列に変換
+        System.out.println(date);
     }
 }
